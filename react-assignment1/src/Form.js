b2f0e1ede9adb4e18744c07a37c9caf078c52fe9
hadiasemi <hasemi@calpoly.edu>
2021-03-29 17:26:56 -0700

    add submit button to the form

--- react-assignment1/src/Form.js
+++ react-assignment1/src/Form.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState(
      {
         name: '',
@@ -19,6 +19,13 @@ function Form() {
         {name: value, job: person['job']}   
       );
    }
+
+
+   function submitForm() {
+      props.handleSubmit(person);
+      setPerson({name: '', job: ''});
+   }
+ 
    return (
       <form>
       <label htmlFor="name">Name</label>
@@ -35,6 +42,9 @@ function Form() {
          id="job"
          value={person.job}
          onChange={handleChange} />
+      <input type="button" value="Submit" onClick={submitForm} />
+
+
       </form>
    );
 
--- react-assignment1/src/MyApp.js
+++ react-assignment1/src/MyApp.js
@@ -11,12 +11,20 @@ function MyApp() {
       });
       setCharacters(updated);
     }
+
+  
+
+function updateList(person) {
+  setCharacters([...characters, person]);
+}
   
 
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form />
+      <Form handleSubmit={updateList} />
+
+
     </div>
   )
 
